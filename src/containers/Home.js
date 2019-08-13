1123b20900d31d79e16a779f7b9cbfd3f873c00d
Shandi Yuwono <shandi.yuwono@yahoo.com>
2019-08-13 20:55:42 +0700

    configured homepage layout

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,11 +1,52 @@
 import React, { Component } from 'react'
 import NavigationBar from '../components/NavigationBar'
+import { Row, Col, Container, Card, Button } from 'react-bootstrap'
+import axios from 'axios'
 
 export default class Home extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            index: []
+        }
+    }
+
+    componentDidMount(){
+        axios.get('./index.json')
+            .then(({data}) => {
+                console.log(data)
+                this.setState({
+                    index: data.data
+                })
+            }) 
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
     render() {
         return (
             <div>
                <NavigationBar /> 
+               <Container>
+                   <Row>
+                       { this.state.index.map(data => {
+                           return (
+                               <Col sm>
+                                   <Card style={{ width: '18rem' }}>
+                                       <Card.Img variant="top" src={data.img} />
+                                       <Card.Body>
+                                           {/* <Card.Title>{data.name.toUpperCase()}</Card.Title> */}
+                                           <a href="#">{data.name.toUpperCase()}</a>
+                                       </Card.Body>
+                                   </Card>
+
+                               </Col>
+                           )
+                       })}
+                  
+                   </Row>
+               </Container>
             </div>
         )
     }
